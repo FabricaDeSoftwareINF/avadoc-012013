57428de617d0fac5d83bfb294e2410e96faf7462
raulbarca@engenhariadesoftware.inf.br <raulbarca@engenhariadesoftware.inf.br@e42956ff-1666-d8d7-014d-b80eeb97584e>
2013-07-13 05:55:04 +0000

    DAO genérico

--- trunk/Desenvolvimento/avadoc-012013/avadoc-controller/src/main/java/br/ufg/inf/avadoc/dao/DocenteDAO.java
+++ trunk/Desenvolvimento/avadoc-012013/avadoc-controller/src/main/java/br/ufg/inf/avadoc/dao/DocenteDAO.java
@@ -1,37 +1,30 @@
 package br.ufg.inf.avadoc.dao;
 
-import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.ufg.inf.avadoc.modelo.Docente;
 
-public class DocenteDAO extends GenericDAO<Docente> {
-	@PersistenceContext (unitName = "docente")
-
-	/**
-	 * Este metodo salva um objeto do tipo {@link Docente} utilizando o
-	 * metodo generico da classe {@link GenericDAO}
-	 * @see GenericDAO
-	 * 
-	 * */
-	@Override
-	public void salvar (Docente docente){
-		super.salvar(docente);
-	}
-
-	/**
-	 * Este metodo exclui um objeto do tipo {@link Docente} pelo id
-	 * utilizando o metodo generico da classe {@link GenericDAO}
-	 * @see GenericDAO
-	 * 
-	 * */
-	public void excluir (int id){
-		Docente T = consultaPorId(id);
-		super.excluir(T);
-	}
-
-	public Docente pesquisarPorNome (String nome){
-		Docente resultado = consultaPorNome(nome);
-		return resultado;
-	}
-
+@Repository
+public class DocenteDAO {
+	private final GenericDAO<Docente> dao;
+	
+    @Autowired
+    public DocenteDAO(final SessionFactory factory) {
+        dao  = new GenericDAO<Docente>(factory, Docente.class);	
+    }
+    
+    @Transactional(readOnly = true)
+    public List<Docente> lista() {
+        return dao.lista();
+    }
+ 
+    @Transactional
+    public void adiciona(final Docente docente) {
+        dao.adiciona(docente);
+    }
 }
