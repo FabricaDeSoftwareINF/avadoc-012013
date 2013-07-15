1bc027bb18f8fc3bf04610277f27fe74996616a4
0wilker0 <0wilker0@gmail.com>
2013-07-15 00:15:48 +0000

    exemplo de integração de service dao e controller implementados e funciando

--- Desenvolvimento/avadoc-012013/avadoc-web/src/main/java/br/ufg/inf/avadoc/controller/AvaliacaoController.java
+++ Desenvolvimento/avadoc-012013/avadoc-web/src/main/java/br/ufg/inf/avadoc/controller/AvaliacaoController.java
@@ -1,23 +1,33 @@
 package br.ufg.inf.avadoc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import br.ufg.inf.avadoc.bean.DocenteBean;
+
 @Scope("session")
 @Component(AvaliacaoController.KEY_COMPONENT)
-public class AvaliacaoController extends Controller{
+public class AvaliacaoController extends Controller {
 
 	public static final String KEY_COMPONENT = "avaliacaoController";
 
 	private static final long serialVersionUID = -7047823076987573930L;
 
-	public void metodo(){
-		
-		for(int i = 0; i < 10000; i++){
-			
+	@Autowired
+	private DocenteBean docenteService;
+
+	public void metodo() {
+
+		for (int i = 0; i < 10000; i++) {
+
 			System.out.println(i);
-			
+
 		}
 	}
-	
+
+	public DocenteBean getDocenteService() {
+		return docenteService;
+	}
+
 }
