898fdfdf92097d343c2512e57b45c52e30d91f2b
rhaissa.nog@gmail.com <rhaissa.nog@gmail.com@e42956ff-1666-d8d7-014d-b80eeb97584e>
2013-07-16 00:42:45 +0000

    Inserindo os códigos dos calculos feitos pelo Rogério

---
+++ trunk/Desenvolvimento/avadoc-012013/avadoc-modelo/src/main/java/br/ufg/inf/avadoc/modelo/calculos/EnumJornadaTrabalho.java
@@ -0,0 +1,9 @@
+package calculos;
+
+public enum EnumJornadaTrabalho {
+
+	jornada20,
+	jornada40,
+	jornadaDE
+
+}
---
+++ trunk/Desenvolvimento/avadoc-012013/avadoc-modelo/src/main/java/br/ufg/inf/avadoc/modelo/calculos/ExcecaoNumeroNegativo.java
@@ -0,0 +1,26 @@
+/**
+ * Sistema de Avaliação do Docente - SAD
+ */
+package calculos;
+
+/**
+ * Exceção lançada caso algum número negativo seja inserido no cálculo das
+ * notas parciais ou finais de estágio probatório ou progressão horizontal.
+ * @see Exception
+ * @author Rogério Tristão Junior
+ * @version 1.0
+ * @since 1.0
+ */
+public final class ExcecaoNumeroNegativo extends Exception {
+
+	/** Serial da exceção. */
+	private static final long serialVersionUID = -3670949586266298144L;
+
+	/**
+	 * Construtor para chamada ao construtor pai.
+	 */
+	public ExcecaoNumeroNegativo() {
+		super("Número negativo não é suportado para esse tipo de cálculo");
+	}
+
+}
