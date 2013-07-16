a1e049d82626402b71f9363adf1bdf091d961d27
Rhaissa.nog <rhaissa.nog@gmail.com>
2013-07-16 00:42:45 +0000

    Inserindo os códigos dos calculos feitos pelo Rogério

---
+++ Desenvolvimento/avadoc-012013/avadoc-modelo/src/main/java/br/ufg/inf/avadoc/modelo/calculos/EnumExigencias.java
@@ -0,0 +1,11 @@
+package calculos;
+
+public enum EnumExigencias {
+
+	ExigenciaI,
+	ExigenciaII,
+	ExigenciaIII,
+	ExigenciaIV,
+	ExigenciaV;
+
+}
---
+++ Desenvolvimento/avadoc-012013/avadoc-modelo/src/main/java/br/ufg/inf/avadoc/modelo/calculos/Nota.java
@@ -0,0 +1,24 @@
+package calculos;
+
+public class Nota {
+
+	private int quantidadeMeses;
+	private double nota;
+
+	public double getNota() {
+		return nota;
+	}
+
+	public int getQuantidadeMeses() {
+		return quantidadeMeses;
+	}
+
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+
+	public void setQuantidadeMeses(int quantidadeMeses) {
+		this.quantidadeMeses = quantidadeMeses;
+	}
+
+}
